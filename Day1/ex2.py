3c98b8fbb7443d955a44d5e619b80a4f3e3aa575
Mark Shillaw <116576050+edu-mryo@users.noreply.github.com>
2023-08-10 23:35:15 +0000

    Day1 -ex4

--- Day1/ex2.py
+++ Day1/ex2.py
@@ -13,4 +13,3 @@ String Concatenation is done with the "+" sign.
 e.g. print("Hello " + "world")
 New lines can be created with a backslash and n.
 ''')
-
---
+++ Day1/ex3.py
@@ -0,0 +1,6 @@
+# Write a program that prints the number of characters in a user's name. 
+# You might need to Google for a function that calculates the length of a string.
+# The screen should show "What if Your Name?". You must input your name in the terminal and the result should return the length of the input
+# eg What is your name? Mark -> 4
+
+print(len(input("What is Your Name? ")))
