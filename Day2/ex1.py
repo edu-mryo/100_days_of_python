961fb390a4d30f259cc9a9a1bddeff7ab57d7349
Mark Shillaw <116576050+edu-mryo@users.noreply.github.com>
2023-08-12 16:27:49 +0000

    fixes

--- Day2/ex1.py
+++ Day2/ex1.py
@@ -1,4 +1,4 @@
-# Write a program that adds the digits in a 2 digit number. 
+# Write a program that adds the digits in a 2 digit number.
 # e.g. if the input was 35, then the output should be 3 + 5 = 8
 
 # 🚨 Don't change the code below 👇
