978ef101896ce064c7deadaf85c77aacfae30b38
Mark Shillaw <116576050+edu-mryo@users.noreply.github.com>
2023-08-22 16:51:00 +0000

    Day7 Challenges from 1 to 4

---
+++ Day7/challenge1.py
@@ -0,0 +1,25 @@
+#Step 1 
+
+import random
+
+word_list = ["aardvark", "baboon", "camel"]
+
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+
+chosen_word = word_list[random.randint(0,len(word_list))]
+#alternate solution
+#chosen_word = random.choice(word_list)
+
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+guess = input("Guess a letter !").lower()
+
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
+for char in chosen_word:
+    if guess == char:
+        print(f"You guessed right: {guess}")
+    else :
+        print("You guessed incorrectly")
+    # print(chosen_word)
+    break
